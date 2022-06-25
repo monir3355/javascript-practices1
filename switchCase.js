748820f5f23481aeb2a35c0a243bbdf8e5baaec2
Md. Monirul Islam <102609792+monir3355@users.noreply.github.com>
2022-06-25 20:15:26 +0600

    adding

---
+++ switchCase.js
@@ -0,0 +1,14 @@
+var letter="q"
+switch (letter){
+    case "A":
+    case "E":
+    case "I":
+    case "O":
+    case "U":
+        console.log("Vowel");
+        break;
+   
+    default:
+        console.log("Consonant");
+       
+}
